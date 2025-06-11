926c4677657fadb0dda25e4b23e769a92fc5fad9
Luis Penafiel Palmer <135802985+LuisPenafiel@users.noreply.github.com>
2025-06-11 16:47:25 +0000

    error fatal en terraform-practice,repetir

--- terraform-practice/day01/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  description = "Nombre único del bucket S3"
-  type        = string
-}
-
-variable "allowed_ip" {
-  description = "IP permitida para acceder al bucket"
-  type        = string
-}
--- terraform-practice/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  region = "eu-central-1"
-  # Usa las credenciales de Codespaces Secrets (ya configuradas)
-}
