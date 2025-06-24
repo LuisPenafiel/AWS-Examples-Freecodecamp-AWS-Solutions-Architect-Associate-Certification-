203f4f468587cd9f946a8d391901ee7df32721b0
Luis Penafiel Palmer <135802985+LuisPenafiel@users.noreply.github.com>
2025-06-24 16:31:36 +0000

    terraform_cloud_backend_config

---
+++ terraform-practices/terraform-cloud-tools/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "luis-terraform-learning"
+    workspaces {
+      name = "terraform-cloud-tools"
+    }
+  }
+}
+
+resource "null_resource" "example" {
+  triggers = {
+    value = "Testing Terraform Cloud"
+  }
+}
